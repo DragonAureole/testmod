17c7b911d2407ca54ebcda75ca9301499e90dd00
DragonAureole <dragonaureole@gmail.com>
2024-12-19 16:38:11 -0300

    again

--- src/main/java/net/dragon/testmod/item/CreativeModTabs.java
+++ src/main/java/net/dragon/testmod/item/CreativeModTabs.java
@@ -1,4 +1,29 @@
 package net.dragon.testmod.item;
 
+import net.dragon.testmod.TestMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
 public class CreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TestMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> TEST_TAB = CREATIVE_MODE_TABS.register("testmod_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(Items.BALL.get()))
+                    .title(Component.translatable("creativetab.test_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(Items.BALL.get());
+                        output.accept(Items.SQUARE.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus)
+    {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
 }
